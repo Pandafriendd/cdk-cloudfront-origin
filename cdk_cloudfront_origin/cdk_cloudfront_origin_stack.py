da7fc3e2bc8c4055fce8955c3aae85a6d110ea63
Li <lizhiyua@38f9d35d438d.ant.amazon.com>
2020-08-28 19:13:32 -0400

    upload test project

--- cdk_cloudfront_origin/cdk_cloudfront_origin_stack.py
+++ cdk_cloudfront_origin/cdk_cloudfront_origin_stack.py
@@ -1,9 +1,7 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins
 )
 
 
@@ -12,13 +10,24 @@ class CdkCloudfrontOriginStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkCloudfrontOriginQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
+        cdnOrigin = core.CfnParameter(self, "cdnOrigin", type="String", description="CDN origin hostname")
 
-        topic = sns.Topic(
-            self, "CdkCloudfrontOriginTopic"
+        cdnDistribution = cloudfront.Distribution(self, "cdnDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                allowed_methods=cloudfront.AllowedMethods.ALLOW_ALL,
+                cached_methods=cloudfront.CachedMethods.CACHE_GET_HEAD,
+                compress=True,
+                forward_query_string=True,
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                origin=origins.HttpOrigin(
+                    domain_name=cdnOrigin.value_as_string,
+                    protocol_policy=cloudfront.OriginProtocolPolicy.HTTPS_ONLY
+                )
+            ),
+            #certificate="<I wish to push here arn string which is store in variable: config.cdnCertArn variable>"
+            #domain_names=['front.' + config.stackName + '.' + config.hostedZone],
+            #comment="CDN distribution for front." + config.stackName + '.' + config.hostedZone,
+            enable_ipv6 = False,
+            #http_version=aws_cloudfront.HttpVersion.HTTP1_1,
+            enabled = True
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
